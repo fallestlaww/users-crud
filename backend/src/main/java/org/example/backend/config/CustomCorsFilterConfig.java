389bcf7cbab2dece7214fd3a14fd6817a3e58aac
Pavlo Svitenko <112775112+fallestlaww@users.noreply.github.com>
2025-06-12 08:35:47 +0300

    Update and rename WebConfig.java to CustomCorsFilterConfig.java

---
+++ backend/src/main/java/org/example/backend/config/CustomCorsFilterConfig.java
@@ -0,0 +1,26 @@
+package org.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CustomCorsFilterConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://courageous-valkyrie-0b3a32.netlify.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        System.out.println("CORS Filter initialized!");
+        return new CorsFilter(source);
+    }
+}
